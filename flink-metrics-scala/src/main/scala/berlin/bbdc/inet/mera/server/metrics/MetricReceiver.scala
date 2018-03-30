208eb6736dc6d733ddd2936b360897c665a5f9ae
Niklas Semmler <nsemmler@inet.tu-berlin.de>
2018-03-30 19:35:45 +0200

    stop traversal, when actor MetricReceiver ends

--- flink-metrics-scala/src/main/scala/berlin/bbdc/inet/mera/server/metrics/MetricReceiver.scala
+++ flink-metrics-scala/src/main/scala/berlin/bbdc/inet/mera/server/metrics/MetricReceiver.scala
@@ -1,9 +1,11 @@
 package berlin.bbdc.inet.mera.server.metrics
 
 import java.io.File
-import java.util.concurrent.{Executors, ScheduledExecutorService, TimeUnit}
+import java.util.concurrent.{Executors, ScheduledExecutorService, ScheduledFuture, TimeUnit}
 
-import akka.actor.{Actor, ActorSystem, Props}
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import berlin.bbdc.inet.mera.message.MetricUpdate
 import berlin.bbdc.inet.mera.server.model._
 import com.typesafe.config.ConfigFactory
@@ -15,8 +17,10 @@ class MetricReceiver(model: Model, mfw : ModelFileWriter) extends Actor {
   val LOG: Logger = LoggerFactory.getLogger("MetricReceiver")
   var first = true
 
+  var traversalFuture : ScheduledFuture[_] = _
+
   override def receive: PartialFunction[Any, Unit] = {
-    case d: MetricUpdate =>
+    case d: MetricUpdate => {
       mfw.updateMetrics(d.timestamp, d.counters.map(t => (t.key, t.count.toDouble)) ++
         d.meters.map(t => (t.key, t.rate)) ++ d.gauges.map(t => (t.key, t.value)))
       modelUpdater.update(d.timestamp,
@@ -31,12 +35,14 @@ class MetricReceiver(model: Model, mfw : ModelFileWriter) extends Actor {
         // Start tra
         val modelTraversal = new ModelTraversal(model, mfw)
         val schd : ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
-        val traversalFuture = schd.scheduleAtFixedRate(modelTraversal, 5, 5, TimeUnit.SECONDS)
+        traversalFuture = schd.scheduleAtFixedRate(modelTraversal, 5, 1, TimeUnit.SECONDS)
       }
+    }
   }
 
   override def postStop(): Unit = {
     mfw.close()
+    traversalFuture.cancel(true)
     super.postStop()
   }
 }
