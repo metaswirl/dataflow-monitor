b1c7c358b89ba328b9b690b5e56b5c9479a655d2
Niklas Semmler <nsemmler@inet.tu-berlin.de>
2018-08-01 14:05:15 +0200

    fixing Akka issues for the nth time

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,4 +1,3 @@
-
 import sbt._
 import Keys._
 
@@ -7,47 +6,53 @@ object Dependencies {
   val specs2Version = "4.0.2"
   val jacksonScalaVersion = "2.9.4"
   val akkaHttpVersion = "10.1.0"
-  val akkaVersion = "2.4.20"
+  val akkaVersion = "2.5.11"
+  val akkaVersionOld = "2.4.20"
   val httpClientVersion = "4.5.5"
 
   val jackson = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonScalaVersion
   val flinkScala = "org.apache.flink" %% "flink-scala" % flinkVersion 
   val flinkStreamScala = "org.apache.flink" %% "flink-streaming-scala" % flinkVersion
-  val flinkMetricsCore = "org.apache.flink" % "flink-metrics-core" % flinkVersion % "provided"
+  val flinkMetricsCore = "org.apache.flink" % "flink-metrics-core" % flinkVersion
   val typesafeConfig = "com.typesafe" % "config" % "1.3.3"
+  val qosLogback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val specs2Core = "org.specs2" %% "specs2-core" % specs2Version % "test"
   val specs2Mock = "org.specs2" %% "specs2-mock" % specs2Version % "test"
   val dropwizardMetrics = "io.dropwizard.metrics" % "metrics-core" % "3.1.0"
-  val qosLogback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val typesafeLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0"
   val typesafeAkkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  val typesafeAkkaActorOld = "com.typesafe.akka" %% "akka-actor" % akkaVersionOld
   val typesafeAkkaHttp = "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
   val typesafeAkkaHttpTestkit = "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % "test"
   val typesafeAkkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   val typesafeAkkaremote = "com.typesafe.akka" %% "akka-remote" % akkaVersion
   val apacheHttpComponents = "org.apache.httpcomponents" % "httpclient" % httpClientVersion
 
-  val commonDependencies: Seq[ModuleID] = Seq(
-    flinkScala, flinkStreamScala, specs2Core, specs2Mock
+  val flinkProvidedDependencies: Seq[ModuleID] = Seq(
+    flinkScala, flinkStreamScala
+  ).map(_ % "provided")
+  val testDependencies: Seq[ModuleID] = Seq(
+    specs2Mock
   )
-  val meraCommonsDependencies: Seq[ModuleID] = Seq(
-    specs2Core, jackson
-  )
-  val useCaseDependencies: Seq[ModuleID] = Seq(
-  ) ++ commonDependencies
-  val templateDependencies: Seq[ModuleID] = Seq(
-    typesafeConfig
-  ) ++ commonDependencies
-  val loadshedderDependencies: Seq[ModuleID] = Seq(
-    typesafeAkkaActor
-  ) ++ commonDependencies
   val monitorDependencies: Seq[ModuleID] = Seq(
     typesafeLogging, typesafeAkkaHttp, typesafeAkkaHttpTestkit,
     typesafeAkkaStream, typesafeAkkaremote, typesafeAkkaActor,
     qosLogback, dropwizardMetrics, apacheHttpComponents, jackson,
-    specs2Core, specs2Mock
-  )
+  ) ++ testDependencies
+
+  // WARNING: Don't risk overlap between Flink's dependencies and your own
+  val meraCommonsDependencies: Seq[ModuleID] = Seq(
+    jackson
+  ) ++ testDependencies
+  val useCaseDependencies: Seq[ModuleID] = Seq(
+  ) ++ flinkProvidedDependencies ++ testDependencies
+  val templateDependencies: Seq[ModuleID] = Seq(
+    typesafeConfig % "provided"
+  ) ++ flinkProvidedDependencies ++ testDependencies
+  val loadshedderDependencies: Seq[ModuleID] = Seq(
+    typesafeAkkaActorOld % "provided"
+  ) ++ flinkProvidedDependencies ++ testDependencies
   val reporterDependencies: Seq[ModuleID] = Seq(
-    flinkScala, flinkStreamScala, flinkMetricsCore, typesafeAkkaActor, qosLogback
-  )
+    typesafeAkkaActorOld, qosLogback, flinkMetricsCore
+  ).map(_ % "provided") ++ flinkProvidedDependencies ++ testDependencies
 }
