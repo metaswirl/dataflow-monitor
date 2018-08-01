cd2a0210df83d53a04a9a90d6001c3a6414582ec
Henrik von Kiedrowski <hkiedrowski@inet.tu-berlin.de>
2018-08-01 15:47:28 +0200

    Fixes after Rebase onto Master

---
+++ mera-monitor/src/main/resources/static/ts/constants.ts
@@ -0,0 +1,13 @@
+export const enum sides{
+    left = "inQueue",
+    right = "outQueue",
+}
+export const inOutPoolResolution = 2;
+export const nodeRadius = 7.5;
+export const sendRecieveIndicator = 60;
+
+const nodeBorder = 1;
+export const arcRadius = {
+    inner: nodeRadius + nodeBorder,
+    outer: (nodeRadius + nodeBorder) * 2
+};
