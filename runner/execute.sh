dd355c07934cbc44675cc25f7ec03be38821b89c
Niklas Semmler <nsemmler@inet.tu-berlin.de>
2018-09-04 14:42:34 +0200

    changes at the runner

--- runner/execute.sh
+++ runner/execute.sh
@@ -7,10 +7,12 @@ if [ $1 -lt 1 ]; then
   echo "should at least run once"
   exit 1
 fi
+
 DATE="$(date +"%Y-%m-%d_%H:%M:%S" )" 
 echo "experiment folder start time: $DATE"
-
 for i in $(seq 1 $1); do
+  TARGET="${DATE}/regular"
+  mkdir -p $TARGET
   ansible-playbook \
   experiment.yml \
   -i hosts \
@@ -18,8 +20,30 @@ for i in $(seq 1 $1); do
   -e '@config/experiment.json' \
   -e '@config/env.json' \
   -e '@config/runtime.json' \
-  -e "experiment"="$DATE" \
   -e "run"=$i \
   -e "flink_commit_id"="661ba399397cce608b0263d8edd202a4726e4622" \
-  -e "mera_commit_id"="fubar"
+  -e "mera_commit_id"="fubar" \
+  -e "experiment"="$TARGET" \
+  -e "optimizer_active"="false"
+  pkill -fu nsemmler mera
+done
+bash stopit.sh
+
+for i in $(seq 1 $1); do
+  TARGET="${DATE}/optimized"
+  mkdir -p $TARGET
+
+  ansible-playbook \
+  experiment.yml \
+  -i hosts \
+  -e '@config/system.json' \
+  -e '@config/experiment.json' \
+  -e '@config/env.json' \
+  -e '@config/runtime.json' \
+  -e "run"=$i \
+  -e "flink_commit_id"="661ba399397cce608b0263d8edd202a4726e4622" \
+  -e "mera_commit_id"="fubar" \
+  -e "experiment"="$TARGET" \
+  -e "optimizer_active"="true"
+  pkill -fu nsemmler mera
 done
